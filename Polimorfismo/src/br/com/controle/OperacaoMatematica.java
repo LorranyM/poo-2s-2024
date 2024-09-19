35400e0ca648e7a52c5e9f472476112eeffac466
LorranyM <141752005+LorranyM@users.noreply.github.com>
2024-09-19 10:36:06 -0300

    Polimorfismo
    Polimorfismo em Java é o princípio pelo qual duas ou mais classes derivadas de uma mesma superclasse podem invocar métodos que têm a mesma identificação (assinatura) mas comportamentos distintos, especializados para cada classe derivada, usando para tanto uma referência a um objeto do tipo da superclasse1. Em Java, isso é alcançado por meio de duas técnicas principais: sobrecarga de métodos e sobrescrita de métodos2. O Polimorfismo permite que os mesmos atributos e objetos sejam utilizados em objetos distintos, com implementações lógicas diferentes.

---
+++ Polimorfismo/src/br/com/controle/OperacaoMatematica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.controle;
+
+/**
+ *
+ * @author lorrany.magalhaes
+ */
+public class OperacaoMatematica {
+    public Double calcular( Double valor1, Double valor2){ //d dado primitivo, D tipo de classe.
+        return 0.0;
+    }
+    
+}
