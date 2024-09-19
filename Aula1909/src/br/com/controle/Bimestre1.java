8d7904cf4b4788680aa2af50dbdd324a499d12b6
LorranyM <141752005+LorranyM@users.noreply.github.com>
2024-09-19 10:34:14 -0300

    Sobrecarga e Sobreescrita em java

---
+++ Aula1909/src/br/com/controle/Bimestre1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.controle;
+
+/**
+ *
+ * @author lorrany.magalhaes
+ */
+public class Bimestre1 extends Semestre{
+    public int media (int valor1, int valor2){
+        return (valor1 +valor2)/2;
+    }
+    
+}
